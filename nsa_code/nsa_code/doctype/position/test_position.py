674bc4c13127eced52af87e7cd302bbbfc428ec7
Eby mathew <abparakunnel@gmail.com>
2022-10-25 07:51:28 +0530

    Fix: update in doctypes suite bay position and level

---
+++ nsa_code/nsa_code/doctype/position/test_position.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Networking Solutions Australia Pty Ltd and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPosition(unittest.TestCase):
+	pass
---
+++ nsa_code/nsa_code/doctype/suite/suite.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Networking Solutions Australia Pty Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Suite', {
+	// refresh: function(frm) {
+
+	// }
+});
